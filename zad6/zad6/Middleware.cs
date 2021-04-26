e6c1d7f28e7eda7f2b42fbad92d455ddb2d61fea
shogi <shogi@DESKTOP-DCG1AN7>
2021-04-26 22:56:03 +0200

    zad6

---
+++ zad6/zad6/Middleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zad6
+{
+    public class Middleware
+    {
+        private RequestDelegate next;
+        public Middleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+
+            if ((browser.Name == BrowserNames.Edge) || (browser.Name == BrowserNames.EdgeChromium) || (browser.Name == BrowserNames.InternetExplorer))
+            {
+                await context.Response
+                      .WriteAsync("Przeglądarka nie jest obsługiwana");
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
---
+++ zad6/zad6/MiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zad6
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder )
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
